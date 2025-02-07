bb14dfe846489d2776012047ff87fa3cd0fa0df5
UmairRathore <umairrathore05@gmail.com>
2025-02-07 16:18:55 +0500

    authentication structure

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
