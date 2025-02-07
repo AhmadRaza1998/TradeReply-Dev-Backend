403f59b779410a9716689ab7fc24a96511e21b39
UmairRathore <umairrathore05@gmail.com>
2025-02-07 12:38:45 +0500

    db engine and kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,16 +4,25 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
+
+    protected $commands = [
+
+    ];
+
+
     /**
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+
     }
+
+
 
     /**
      * Register the commands for the application.
